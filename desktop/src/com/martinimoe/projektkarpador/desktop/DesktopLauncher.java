cd95540c8e095a5c2cd85d77d603a0b9779f6380
MartiniMoe <martini.moe@cheex.de>
2014-09-12 00:51:02 +0200

    initial libGDX project

---
+++ desktop/src/com/martinimoe/projektkarpador/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.martinimoe.projektkarpador.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.martinimoe.projektkarpador.ProjektKarpador;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProjektKarpador(), config);
+	}
+}
