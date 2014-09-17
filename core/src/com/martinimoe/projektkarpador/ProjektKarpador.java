e2be49cb3d84bea7ef41c3356054aab11225d936
MartiniMoe <martini.moe@cheex.de>
2014-09-17 17:48:41 +0200

    merge commit

--- core/src/com/martinimoe/projektkarpador/ProjektKarpador.java
+++ core/src/com/martinimoe/projektkarpador/ProjektKarpador.java
@@ -1,9 +1,11 @@
 package com.martinimoe.projektkarpador;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
@@ -11,12 +13,20 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.utils.Scaling;
 
-public class ProjektKarpador extends ApplicationAdapter {
-	SpriteBatch batch;
+public class ProjektKarpador extends ApplicationAdapter implements ApplicationListener, InputProcessor {
 	AtlasRegion grassLeft;
 	AtlasRegion grassMiddle;
 	AtlasRegion grassRight;
 	AtlasRegion playerStanding;
+
+	
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private int imgX = 20;
+	private int imgY = 70;
+	private boolean imgMove = false;
+	private int imgMoveOffsetX = 0;
+	private int imgMoveOffsetY = 0;
 	
 	@Override
 	public void create () {
@@ -28,6 +38,14 @@ public class ProjektKarpador extends ApplicationAdapter {
 		grassMiddle = atlas.findRegion("Tiles/grassMid");
 		grassRight = atlas.findRegion("Tiles/grassRight");
 		playerStanding = atlas.findRegion("Player/p1_stand");
+		
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+		
+		camera = new OrthographicCamera(w, h);
+		camera.setToOrtho(false, w * 100, h * 100);
+		
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
@@ -38,8 +56,96 @@ public class ProjektKarpador extends ApplicationAdapter {
 		batch.draw(grassLeft, 0, 0);
 		batch.draw(grassMiddle, 70, 0);
 		batch.draw(grassRight, 140, 0);
-		batch.draw(playerStanding, 20, 70);
+		batch.draw(playerStanding, imgX, imgY);
 		batch.end();
+	}
+	
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		int w = Gdx.graphics.getWidth();
+		int h = Gdx.graphics.getHeight();
 		
+		if (screenX >= imgX && screenX < imgX + playerStanding.packedWidth &&
+		    (h - screenY) >= imgY && (h - screenY) < imgY + playerStanding.packedHeight)
+		{
+			imgMove = true;
+			
+			imgMoveOffsetX = screenX - imgX;
+			imgMoveOffsetY = (h - screenY) - imgY;
+		}
+		
+		System.out.println("pos: " + screenX + ", " + screenY);
+		
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		imgMove = false;
+		
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		if (imgMove)
+		{
+			int w = Gdx.graphics.getWidth();
+			int h = Gdx.graphics.getHeight();
+			
+			imgX = screenX - imgMoveOffsetX;
+			imgY = (h - screenY) - imgMoveOffsetY;
+		}
+		
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
