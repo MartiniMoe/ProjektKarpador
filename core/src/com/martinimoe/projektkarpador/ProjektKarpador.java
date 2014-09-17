8b8983f0af27576475ebc977cd9c87ebd5848cce
MartiniMoe <martini.moe@cheex.de>
2014-09-17 17:30:20 +0200

    Texturen und TextureAtlas

--- core/src/com/martinimoe/projektkarpador/ProjektKarpador.java
+++ core/src/com/martinimoe/projektkarpador/ProjektKarpador.java
@@ -5,26 +5,40 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import com.badlogic.gdx.utils.Scaling;
 
 public class ProjektKarpador extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	AtlasRegion grassLeft;
+	AtlasRegion grassMiddle;
+	AtlasRegion grassRight;
+	AtlasRegion playerStanding;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 		
-		
-		
+		TextureAtlas atlas;
+		atlas = new TextureAtlas(Gdx.files.internal("KarpadorAtlas.pack"));
+		grassLeft = atlas.findRegion("Tiles/grassLeft");
+		grassMiddle = atlas.findRegion("Tiles/grassMid");
+		grassRight = atlas.findRegion("Tiles/grassRight");
+		playerStanding = atlas.findRegion("Player/p1_stand");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(grassLeft, 0, 0);
+		batch.draw(grassMiddle, 70, 0);
+		batch.draw(grassRight, 140, 0);
+		batch.draw(playerStanding, 20, 70);
 		batch.end();
 		
 	}
