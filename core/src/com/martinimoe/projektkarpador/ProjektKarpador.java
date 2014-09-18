6708a9cbc5d8954dd3bdc1d15e1d11ff3c314d00
derhannesb <johannes.bade@gmail.com>
2014-09-18 19:19:05 +0200

    Removed unnecessary import

--- core/src/com/martinimoe/projektkarpador/ProjektKarpador.java
+++ core/src/com/martinimoe/projektkarpador/ProjektKarpador.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.PolygonRegion;
 import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
