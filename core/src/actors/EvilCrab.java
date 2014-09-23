7c480ebfe70e13d24c0d3f66a835e8899de6d7d8
MartiniMoe <martini.moe@cheex.de>
2014-09-23 16:20:55 +0200

    Added EvilCrab

---
+++ core/src/actors/EvilCrab.java
@@ -0,0 +1,12 @@
+package actors;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.physics.box2d.Body;
+
+public class EvilCrab {
+	private Animation walk = null;
+	private float elapsed = 0;
+	private Body body = null;
+	
+	
+}
