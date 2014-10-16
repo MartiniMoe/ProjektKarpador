d26ec502cf413ab034336007ce6b7b7e691e04b6
MartiniMoe <martini.moe@cheex.de>
2014-10-16 14:08:20 +0200

    Cat can turn

--- core/src/actors/Cat.java
+++ core/src/actors/Cat.java
@@ -1,8 +1,10 @@
 package actors;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
@@ -11,6 +13,8 @@ import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Array.ArrayIterator;
 import com.martinimoe.projektkarpador.Config;
 import com.martinimoe.projektkarpador.GameContext;
 
@@ -47,6 +51,28 @@ public class Cat extends Enemy{
 		setDamage(100f);
 	}
 	
+	private float animationSpeed = 0f;
+	private Array<AtlasRegion> frames = null;
+	private Array<AtlasRegion> framesFlip = null;
+	@Override
+	protected void loadTexture(String regionName, float animationSpeed, PlayMode playMode) {
+		this.animationSpeed = animationSpeed;
+		// Textur laden
+		frames = gameContext.getAtlas().findRegions(regionName);
+		framesFlip = gameContext.getAtlas().findRegions(regionName);
+		
+		ArrayIterator<AtlasRegion> ai = new ArrayIterator<AtlasRegion>(framesFlip);
+		while (ai.hasNext()) {
+			ai.next().flip(true, false);
+		}
+		
+		move = new Animation(animationSpeed, frames);
+		move.setPlayMode(playMode);
+		// Größe aus Textur generieren
+		setWidth(frames.get(0).getRegionWidth());
+		setHeight(frames.get(0).getRegionHeight());
+	}
+	
 	private float jumpDelay = 0f;
 	@Override
 	public void act(float delta) {
@@ -57,9 +83,11 @@ public class Cat extends Enemy{
 		
 		if (this.getX() > gameContext.getFish().getX() && speed > 0) {
 			speed = speed * -1;
+			move = new Animation(animationSpeed, framesFlip);
 		}
 		if (this.getX() < gameContext.getFish().getX() && speed < 0){
 			speed = speed * -1;
+			move = new Animation(animationSpeed, frames);
 		}
 		
 		if (jumpDelay > 5f) {
